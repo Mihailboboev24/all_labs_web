7cac8d58e3c238eeeea51b791cd4113f9b62405a
SergoSektor <serg.sektor.droid@gmail.com>
2024-05-31 17:41:38 +0700

    вывод текущей даты

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -15,6 +15,21 @@ function showDate() {
     for (const [country, locale] of Object.entries(locales)) {
         output += `<p><strong>Дата и время для локали ${country}:</strong> ${today.toLocaleString(locale)}</p>`;
     }
-
     document.getElementById('locale-dates').innerHTML = output;
+
+    const weekDays = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+    const months = ["январь", "февраль", "март", "апрель", "май", "июнь", "июль", "август", "сентябрь", "октябрь", "ноябрь", "декабрь"];
+
+    const currentYear = today.getFullYear();
+    const currentMonth = months[today.getMonth()];
+    const currentDate = today.getDate();
+    const currentDay = weekDays[today.getDay()];
+
+    const dateInfo = `
+        <p>Текущий год: ${currentYear}</p>
+        <p>Текущий месяц: ${currentMonth}</p>
+        <p>Текущая дата: ${currentDate}</p>
+        <p>День недели: ${currentDay}</p>
+    `;
+    document.getElementById('date-info').innerHTML = dateInfo;
 }
