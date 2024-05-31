5f77cc348881349f698ccb0c99966228ac47f63f
SergoSektor <serg.sektor.droid@gmail.com>
2024-05-31 18:00:49 +0700

    Количество дней с даты рождения

---
+++ lab8/get-days-count.js
@@ -0,0 +1,9 @@
+function showDaysCount() {
+    let today = new Date();
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date(inputDate.value);
+    let daysCount = (today - birthday) / 1000 / 60 / 60 / 24;
+    daysCount = Math.floor(daysCount);
+
+    document.getElementById('days-count').innerText = `Число дней: ${daysCount}`;
+}
